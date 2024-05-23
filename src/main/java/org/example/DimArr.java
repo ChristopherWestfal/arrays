ceffaaf854b15c17a8bb1cdc1b22b30fce9e77ff
ChrizDope <incubusid@googlemail.com>
2024-05-23 12:01:19 +0200

    Add Files

---
+++ src/main/java/org/example/DimArr.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class DimArr {
+    public static void main(String[] args) {
+
+        int[][] multArray = new int[2][2];
+
+        for(int i = 0; i < multArray.length; i++) {
+            for(int j = 0; j < multArray[i].length; j++) {
+
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int[] array2 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         int[] array3 = new int[10];
 
+
         // Aufgabe 1 - 2
         int sum = 0;
 
